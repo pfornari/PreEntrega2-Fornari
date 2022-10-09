fac0c18d2d6570bdad261c62398f216e849c7890
pfornari <pedro@fornari.org>
2022-10-08 22:01:27 -0300

    Segunda Entrega

--- src/Widget/Widget.jsx
+++ src/Widget/Widget.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Badge } from 'react-bootstrap'
+import {FaShoppingCart} from 'react-icons/fa'
 
 function Widget() {
     return (
         <>
+          <FaShoppingCart size='1.5rem'/>
+          <Badge pill bg="danger">
             Carrito
+          </Badge>
         </>
     )
 }
-
 export default Widget
